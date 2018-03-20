8aceaba2e15b9cd770b21ae6e4533d335ed06a65
Stephen Tweeddale <stephen.tweeddale@computerminds.co.uk>
2018-03-20 14:25:15 +0000

    Update 4xl text size

--- tailwind.js
+++ tailwind.js
@@ -186,7 +186,7 @@ module.exports = {
     'xl': '1.25rem',    // 20px
     '2xl': '1.5rem',    // 24px
     '3xl': '1.875rem',  // 30px
-    '4xl': '2.25rem',   // 36px
+    '4xl': '2.5rem',    // 40px
     '5xl': '3rem',      // 48px
   },
 
