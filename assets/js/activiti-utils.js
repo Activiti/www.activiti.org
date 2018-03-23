01b9f687c3332295f1c7431b55e01263e54a6ac6
James Williams <james.williams@computerminds.co.uk>
2018-03-23 17:36:59 +0000

    Work on marketo modal.

---
+++ assets/js/activiti-utils.js
@@ -0,0 +1,77 @@
+// Compatibility layer copying javascript functions from Drupal's drupal.js.
+
+var Activiti = Activiti || {};
+
+(function (Activiti) {
+
+  /**
+   * Encode special characters in a plain-text string for display as HTML.
+   *
+   * @ingroup sanitization
+   */
+  Activiti.checkPlain = function (str) {
+    var character, regex,
+      replace = { '&': '&amp;', "'": '&#39;', '"': '&quot;', '<': '&lt;', '>': '&gt;' };
+    str = String(str);
+    for (character in replace) {
+      if (replace.hasOwnProperty(character)) {
+        regex = new RegExp(character, 'g');
+        str = str.replace(regex, replace[character]);
+      }
+    }
+    return str;
+  };
+
+  /**
+   * Replace substring.
+   *
+   * The longest keys will be tried first. Once a substring has been replaced,
+   * its new value will not be searched again.
+   *
+   * @param {String} str
+   *   A string with placeholders.
+   * @param {Object} args
+   *   Key-value pairs.
+   * @param {Array|null} keys
+   *   Array of keys from the "args".  Internal use only.
+   *
+   * @return {String}
+   *   Returns the replaced string.
+   */
+  Activiti.stringReplace = function (str, args, keys) {
+    if (str.length === 0) {
+      return str;
+    }
+
+    // If the array of keys is not passed then collect the keys from the args.
+    if (!$.isArray(keys)) {
+      keys = [];
+      for (var k in args) {
+        if (args.hasOwnProperty(k)) {
+          keys.push(k);
+        }
+      }
+
+      // Order the keys by the character length. The shortest one is the first.
+      keys.sort(function (a, b) { return a.length - b.length; });
+    }
+
+    if (keys.length === 0) {
+      return str;
+    }
+
+    // Take next longest one from the end.
+    var key = keys.pop();
+    var fragments = str.split(key);
+
+    if (keys.length) {
+      for (var i = 0; i < fragments.length; i++) {
+        // Process each fragment with a copy of remaining keys.
+        fragments[i] = Activiti.stringReplace(fragments[i], args, keys.slice(0));
+      }
+    }
+
+    return fragments.join(args[key]);
+  };
+
+})(Activiti);
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const browserSync = require("browser-sync").create();
 const child = require("child_process");
+const dest = require('gulp-dest');
 const gulp = require("gulp");
 const gutil = require('gulp-util');
 const Q = require('q');
+const merge = require('merge-stream');
 
 const cssFiles = "src/**/*.css";
 const siteRoot = "_site";
@@ -44,7 +46,7 @@ const jekyllTask = function (watch = false) {
 /**
  * Build Site
  */
-gulp.task("build", ['css'], function() {
+gulp.task("build", ['css', 'js'], function() {
   return jekyllTask();
 });
 
@@ -53,6 +55,20 @@ gulp.task("build", ['css'], function() {
  */
 gulp.task("jekyll:watch", function() {
   return jekyllTask(true);
+});
+
+/**
+ * Compile JS
+ */
+gulp.task('js', function () {
+  var merged = merge();
+  var files = {
+    'jquery/dist/jquery.min.js': 'jquery-min.js',
+  };
+  Object.keys(files).forEach(function(file) {
+    merged.add(gulp.src(`node_modules/${file}`).pipe(dest('', {basename: `${files[file]}`})).pipe(gulp.dest(`assets/js`)));
+  });
+  return merged;
 });
 
 /**
@@ -83,7 +99,7 @@ gulp.task("css", function() {
 /**
  * Serve site with Browsersync
  */
-gulp.task("serve", ["css"], () => {
+gulp.task("serve", ["css", "js"], () => {
   browserSync.init({
     files: [siteRoot + "/**"],
     open: "local",
@@ -100,4 +116,4 @@ gulp.task("serve", ["css"], () => {
 });
 
 gulp.task("default", ['build']);
-gulp.task("watch", ['jekyll:watch', 'css', 'serve']);
+gulp.task("watch", ['jekyll:watch', 'css', 'js', 'serve']);
