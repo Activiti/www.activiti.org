8b52bf14f3ef71816cd8ddc97e65c2625d6bf73f
James Williams <james.williams@computerminds.co.uk>
2018-03-26 10:55:29 +0100

    Fixed gulpfile, sorry!

--- gulpfile.js
+++ gulpfile.js
@@ -62,10 +62,10 @@ gulp.task("jekyll:watch", function() {
  */
 gulp.task('js', function () {
   var merged = merge();
-  var files = {
+  var files = [
     'jquery/dist/jquery.min.js',
     'jquery.cookie/jquery.cookie.js',
-  };
+  ];
   files.forEach(function(file) {
     merged.add(gulp.src(`node_modules/${file}`).pipe(gulp.dest(`assets/js`)));
   });
